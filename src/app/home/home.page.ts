3b6382f9ce2535066aa3640f7100cab5d93ae15a
VictorPresenca <presenca.victor@gmail.com>
2025-02-16 18:45:05 -0300

    testando colocar o src no repositório

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonRange } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  peso: string='';
+  altura: string= '';
+  sexo: string= '';
+  userInput: string = '';
+
+  resp1: string = '';
+  imc: string = '';
+  massa: string = '';
+  gcb: string = '';
+  gorduraCorporal: string = '';
+  saudeGeral: string = '';
+
+
   constructor() {}
+
+  botaoSaude() {
+
+
+
+  }
+
+//   IMC com a categoria correspondente
+//   Massa Muscular estimada
+//   Gasto Calórico Basal (GCB)
+//   Percentual de Gordura Corporal
+//   Análise de saúde geral com recomendações
 
 }
